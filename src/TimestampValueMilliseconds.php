31f6a09a37eafc9ae4d06b0afe95e7cbd8864a15
Mirko Bukilic <mirko.bukilic@codeplicity.com>
2025-05-28 15:32:46 +0200

    Added TimestampValueMilliseconds for handling milliseconds

---
+++ src/TimestampValueMilliseconds.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace G4\ValueObject;
+
+use G4\ValueObject\Exception\MissingTimestampValueException;
+use G4\ValueObject\Exception\InvalidTimestampValueException;
+
+class TimestampValueMilliseconds implements StringInterface, NumberInterface
+{
+    const TIMESTAMP_MILLISECONDS_LENGTH = 13;
+
+    /**
+     * @var int
+     */
+    private $value;
+
+    /**
+     * @param $value
+     * @throws MissingTimestampValueException
+     * @throws InvalidTimestampValueException
+     */
+    public function __construct($value)
+    {
+        if (empty($value)) {
+            throw new MissingTimestampValueException();
+        }
+
+        if (!self::isValid($value)) {
+            throw new InvalidTimestampValueException($value);
+        }
+
+        $this->value = (int) $value;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return (string) $this->value;
+    }
+
+    /**
+     * @return int
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * @return int
+     */
+    public function getSeconds()
+    {
+        return (int) ($this->value / 1000);
+    }
+
+    /**
+     * @param string $format
+     * @return string
+     */
+    public function getFormatted($format = 'Y-m-d')
+    {
+        return date($format, $this->getSeconds());
+    }
+
+    /**
+     * @return self
+     * @throws InvalidTimestampValueException
+     * @throws MissingTimestampValueException
+     */
+    public static function now()
+    {
+        return new self((int) (microtime(true) * 1000));
+    }
+
+    public static function isValid($timestamp)
+    {
+        if (strlen((string) $timestamp) !== self::TIMESTAMP_MILLISECONDS_LENGTH) {
+            return false;
+        }
+
+        $check = (is_int($timestamp) || is_float($timestamp))
+            ? $timestamp
+            : (string) (int) $timestamp;
+
+        if ($check !== $timestamp) {
+            return false;
+        }
+
+        $intValue = (int) $timestamp;
+
+        return $intValue >= 0;
+    }
+}
---
+++ tests/unit/src/TimestampValueMillisecondsTest.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace unit\src;
+
+use G4\ValueObject\Exception\MissingTimestampValueException;
+use G4\ValueObject\Exception\InvalidTimestampValueException;
+use G4\ValueObject\TimestampValueMilliseconds;
+use PHPUnit\Framework\TestCase;
+
+class TimestampValueMillisecondsTest extends TestCase
+{
+    /**
+     * @return void
+     */
+    public function testMissingValueException()
+    {
+        $this->expectException(MissingTimestampValueException::class);
+        new TimestampValueMilliseconds('');
+    }
+
+    /**
+     * @return void
+     */
+    public function testInvalidValueFloatException()
+    {
+        $this->expectException(InvalidTimestampValueException::class);
+        new TimestampValueMilliseconds('1.0');
+    }
+
+    /**
+     * @return void
+     */
+    public function testInvalidValueHexaNumberException()
+    {
+        $this->expectException(InvalidTimestampValueException::class);
+        new TimestampValueMilliseconds('0xFF');
+    }
+
+    /**
+     * @return void
+     */
+    public function testValidValues()
+    {
+        $now = (int) (microtime(true) * 1000);
+        $timestampInt = new TimestampValueMilliseconds($now);
+        $this->assertEquals($now, $timestampInt->getValue());
+
+        $timestampString = new TimestampValueMilliseconds('1748436857881');
+        $this->assertEquals('1748436857881', (string) $timestampString);
+    }
+
+    /**
+     * @return void
+     */
+    public function testGetFormatted()
+    {
+        $timestamp = new TimestampValueMilliseconds('1748436857881');
+        $this->assertEquals('2025-05-28', $timestamp->getFormatted());
+        $this->assertEquals('May 28 / 2:54', $timestamp->getFormatted('F j / g:i'));
+    }
+
+    /**
+     * @return void
+     */
+    public function testGetSeconds()
+    {
+        $timestamp = new TimestampValueMilliseconds(1748436857881);
+        $this->assertEquals(1748436857, $timestamp->getSeconds());
+    }
+
+    /**
+     * @return void
+     */
+    public function testNow()
+    {
+        $time = time();
+        $timestamp = TimestampValueMilliseconds::now();
+        $this->assertEquals('2025-05-28', $timestamp->getFormatted());
+        $this->assertEquals($time, $timestamp->getSeconds());
+    }
+
+    /**
+     * @return void
+     */
+    public function testInvalidValueLength()
+    {
+        $this->expectException(InvalidTimestampValueException::class);
+        new TimestampValueMilliseconds(1748436857);
+    }
+}
